aa14f8358d57c5eedb89f7b38bb77de07e12b532
Ghazali <shazebansari2536@gmail.com>
2024-10-19 00:54:46 +0530

    Made changes

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,39 +1,45 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import photo from '../assets/photo.avif'
+import { Link } from 'react-router-dom';
 export const HomeContent = () => {
   const isReverseLayout = location.pathname === '/about';
   console.log(isReverseLayout)
   return (
-    <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'} bg-white w-screen px-4 md:px-16 lg:px-56 py-12 lg:py-[60px]`}>
-    <motion.h1
-      initial={{ opacity: 0, y: 50 }} 
-      animate={{ opacity: 1, y: 0 }}  
-      transition={{ duration: 1.0, ease: "easeOut" }} 
-    >
-      <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'} `}>
-      <h3 className='mt-4 text-[24px] md:text-[30px] sm:text-[20px] font-light'>
-      Now, as a creative entrepreneur, I help others find their voice. I also lead 'sustenance.community,' a space dedicated to supporting and empowering women who want to live a modest, meaningful life
-      </h3>
-      <h3 className='text-[24px] md:text-[30px] sm:text-[20px] font-light'>
-        My journey started in Bollywood, where I portrayed characters on screen that touched many hearts. But in 2019, I chose a different path, a path that aligned with my deeper values, personal growth, and faith. I stepped away from the spotlight to live a life true to myself.
-      </h3>
+    <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'} bg-white w-auto px-4 md:px-16 lg:px-56 py-12 lg:py-[60px]`}>
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.0, ease: "easeOut" }}
+      >
+        <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'} `}>
+          <div className='flex flex-col space-y-6'>
+            <h3 className='mt-4 text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
+              For years, I expressed myself through storytelling in Bollywood. I connected deeply with the characters I portrayed, but over time, I felt a shift within me. Despite how well I fit into that world, I realized it wasn’t where I truly belonged.
+            </h3>
+            <h3 className='text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
+              In 2019, I stepped away, feeling the path no longer resonated with my true self. What I thought was success had distanced me from my inner peace. As I stepped back, I began a journey of rediscovering my purpose, one filled with reflection, growth, and a search for contentment.
+            </h3>
+            <h3 className={`${!isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed`}>
+              Through this ongoing journey, I’ve gained a new sense of purpose. Gradually, I became drawn to ideas that aligned with my evolving values. Now, I’m building Sustenance—a place for women to come together, find support, and share meaningful conversations rooted in faith and growth. It’s the kind of community I wish I had when navigating my own path.
+            </h3>
+          </div>
 
-  
-      <h3 className={`mt-8 ${isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[24px] font-light`}>
-        About me
-      </h3>
-  
-      <div className={`flex ${isReverseLayout ? 'mb-16' : ''}  justify-end mt-8`}>
-        <img
-          className='pic-img rounded-full  lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] object-cover'
-          src={photo}
-          alt="About me"
-        />
-      </div>
-      </div>
-    </motion.h1>
-  </div>
+          <Link to={'/about'}>
+            <h3 className={`mt-8 ${isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[22px] font-light`}>
+              About me
+            </h3>
+          </Link>
+          <div className={`flex ${isReverseLayout ? 'mb-16' : ''}  justify-end mt-8`}>
+            <img
+              className='pic-img rounded-full  lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] object-cover'
+              src={photo}
+              alt="About me"
+            />
+          </div>
+        </div>
+      </motion.h1>
+    </div>
   )
 }
 
