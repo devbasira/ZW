d7701d5822f80a9ed434cf9e9f803553ed51edc0
Ghazali <shazebansari2536@gmail.com>
2024-11-12 20:19:07 +0530

    Font defined

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,22 @@ export const Footer = () => {
                 <div className='flex gap-2 flex-col md:flex-row items-center'>
                     <h3 className='text-[#A4A4A4] text-lg'>powered by </h3>
                     <Link to={'https://basira.studio/'}>
-                        <h3 className='text-lg'>Basira</h3>
+                        <h3 className='text-lg hover:text-[#2226bc]'>Basira</h3>
                     </Link>
                 </div>
                 <div className='flex flex-col md:flex-row items-center'>
                     <h3 className='text-[#A4A4A4] text-lg'>Get in touch at</h3>
-                    <h3 className='ml-2 text-lg'>contact@zairawasim.com</h3>
+                    <a
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@zairawasim.com&su=Your%20Subject&body=Your%20Message"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='ml-2 text-lg hover:text-[#2226bc] hover:cursor-pointer'
+                    >
+                        contact@zairawasim.com
+                    </a>
                 </div>
+
+
                 {/* <div className='flex flex-col md:flex-row items-center'>
                     <h3 className={`${isReverseLayout ? 'hidden' : 'flex'} text-lg`}>Resume</h3>
                 </div> */}
