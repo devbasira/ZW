d37589025674e4ec28757e46782c2b2f6511a5e0
Ghazali <shazebansari2536@gmail.com>
2024-11-12 20:09:55 +0530

    Font defined

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import { motion } from 'framer-motion';
-import photo from '../assets/home_image.jpg'
-import photo_about from '../assets/about_image.jpg'
+import photo_black from '../assets/photo_black.jpg'
 import logo from '../assets/ZW_logo.png'
 import { Link, useLocation } from 'react-router-dom';
 
@@ -18,11 +17,16 @@ export const HomeContent = () => {
         transition={{ duration: 1.0, ease: "easeOut" }}
       >
         <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'}`}>
-          <div className='flex flex-col space-y-6'>
-            <h3 className='mt-4 text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
-              For years, I expressed myself through storytelling in Bollywood. I connected deeply with the characters I portrayed, but over time, I felt a shift within me. Despite how well I fit into that world, I realized it wasn’t where I truly belonged.
+          <div className='flex flex-col space-y-6 lg:mr-64' >
+            <div>
+            <h3 className='mt-4 mb-3 text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
+              For years, I expressed myself through storytelling in Bollywood.
             </h3>
-            <h3 className='text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
+            <h3 className=' text-[20px] md:text-[24px] -mt-4 sm:text-[20px] font-light leading-relaxed'>
+            I connected deeply with the characters I portrayed, but over time, I felt a shift within me. Despite how well I fit into that world, I realized it wasn’t where I truly belonged.
+            </h3>
+            </div>
+            <h3 className=' text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
               In 2019, I stepped away, feeling the path no longer resonated with my true self. What I thought was success had distanced me from my inner peace. As I stepped back, I began a journey of rediscovering my purpose, one filled with reflection, growth, and a search for contentment.
             </h3>
             <h3 className={`${!isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed`}>
@@ -30,23 +34,23 @@ export const HomeContent = () => {
             </h3>
           </div>
           <Link to={'/about'}>
-            <h3 className={`mt-8 ${isReverseLayout ? 'hidden' : 'flex'} text-[#A4A4A4] text-[20px] md:text-[22px] font-light`}>
+            <h3 className={`mt-8 ${isReverseLayout ? 'hidden' : 'flex'} text-[#A4A4A4] text-[20px] md:text-[22px] font-light hover:text-[#2226bc]`}>
               Read more...
             </h3>
           </Link>
           <div className={`flex ${isReverseLayout ? 'mb-16' : ''} justify-end mt-8`}>
             {isReverseLayout ? (
               <img
-              className="pic-img rounded-full object-contain border lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[250px] h-[250px]"
+              className="pic-img rounded-full object-fit border lg:w-[400px] lg:h-[400px] md:w-[300px] md:h-[300px] sm:w-[200px] sm:h-[200px] w-[150px] h-[150px]"
               src={logo} 
               alt="Logo"
             />
             ) : ( <img
               className="pic-img rounded-full object-cover lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[250px] h-[250px]"
-              src={isHovered ? photo : photo_about} 
+              src={photo_black} 
               alt="About me"
-              onMouseEnter={() => setIsHovered(true)} 
-              onMouseLeave={() => setIsHovered(false)} 
+              // onMouseEnter={() => setIsHovered(true)} 
+              // onMouseLeave={() => setIsHovered(false)} 
             />)}          
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo_black from '../assets/logo_black.svg';
+import logo_yellow from '../assets/logo_yellow.svg';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const [isHovered, setIsHovered] = useState(false);
   const isReverseLayout = location.pathname === '/about';
-
   return (
     <div className='z-10 bg-white w-full sticky top-0 px-6 py-4 md:px-12 lg:px-28 4k:px-0 flex items-center justify-center border-b border-b--gray-600'>
       <div className='w-full max-w-[1800px] flex items-center justify-between'>
         <div onClick={() => navigate('/')} className='flex items-center gap-2 cursor-pointer'>
-          <img className='w-[35px] h-[35px] mx-4' src={logo_black} alt="" />
+          <img className='w-[35px] h-[35px] mx-4' src={isHovered ? logo_yellow : logo_black} alt="" onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)} />
           <div className="flex flex-col">
             <h2 className="font-bold">Zaira Wasim</h2>
             <h2 className="text-[#A4A4A4] -mt-1">Creative Entrepreneur</h2>
