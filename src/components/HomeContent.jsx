15a9f86af0faafa2571bcc99f37ff06ccff783b4
Ghazali <shazebansari2536@gmail.com>
2024-11-12 15:10:05 +0530

    changed About page

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { motion } from 'framer-motion';
 import photo from '../assets/home_image.jpg'
 import photo_about from '../assets/about_image.jpg'
+import logo from '../assets/ZW_logo.png'
 import { Link, useLocation } from 'react-router-dom';
 
 export const HomeContent = () => {
   const location = useLocation();
   const isReverseLayout = location.pathname === '/about';
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'} bg-white w-full max-w-[1200px] mx-auto px-4 md:px-16  py-12 lg:py-[60px] 4k:px-0`}>
@@ -33,11 +35,19 @@ export const HomeContent = () => {
             </h3>
           </Link>
           <div className={`flex ${isReverseLayout ? 'mb-16' : ''} justify-end mt-8`}>
-            <img
-              className='pic-img rounded-full object-cover lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[250px] h-[250px]' // Add default width and height for smaller screens
-              src={(isReverseLayout ?? false) ? photo_about : photo}
-              alt="About me"
+            {isReverseLayout ? (
+              <img
+              className="pic-img rounded-full object-contain lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[250px] h-[250px]"
+              src={logo} 
+              alt="Logo"
             />
+            ) : ( <img
+              className="pic-img rounded-full object-cover lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[250px] h-[250px]"
+              src={isHovered ? photo : photo_about} 
+              alt="About me"
+              onMouseEnter={() => setIsHovered(true)} 
+              onMouseLeave={() => setIsHovered(false)} 
+            />)}          
           </div>
         </div>
       </motion.h1>
