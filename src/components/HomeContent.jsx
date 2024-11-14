2f70cd374eb3ef506c467cf1c857d8c1adfa3447
Ghazali <shazebansari2536@gmail.com>
2024-11-14 22:28:54 +0530

    Added photo

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { motion } from 'framer-motion';
 import photo_black from '../assets/photo_black.jpg'
+import ZW_Yellow from '../assets/ZW_Yellow.png'
 import logo from '../assets/ZW_logo.png'
 import { Link, useLocation } from 'react-router-dom';
 
@@ -19,21 +20,21 @@ export const HomeContent = () => {
         <div className={`flex ${isReverseLayout ? 'flex-col-reverse' : 'flex-col'}`}>
           <div className='flex flex-col space-y-6 lg:mr-64 lg:px-0 md:px-0 sm:px-0 px-10' >
             <div>
-            <h3 className='mt-4 mb-3 text-[20px] md:text-[24px] sm:text-[20px]  leading-relaxed'>
+            <h3 className='mt-4 mb-3 text-[20px] md:text-[24px] sm:text-[20px] font-light  leading-relaxed'>
               For years, I expressed myself through storytelling in Bollywood.
             </h3>
-            <h3 className=' text-[20px] md:text-[24px] -mt-4 sm:text-[20px]  leading-relaxed'>
+            <h3 className=' text-[20px] md:text-[24px] -mt-4 sm:text-[20px] font-light leading-relaxed'>
             I connected deeply with the characters I portrayed, but over time, I felt a shift within me. Despite how well I fit into that world, I realized it wasn’t where I truly belonged.
             </h3>
             </div>
-            <h3 className=' text-[20px] md:text-[24px] sm:text-[20px]  leading-relaxed'>
+            <h3 className=' text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed'>
               In 2019, I stepped away, feeling the path no longer resonated with my true self. What I thought was success had distanced me from my inner peace. As I stepped back, I began a journey of rediscovering my purpose, one filled with reflection, growth, and a search for contentment.
             </h3>
-            <h3 className={`${!isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[24px] sm:text-[20px]  leading-relaxed`}>
+            <h3 className={`${!isReverseLayout ? 'hidden' : 'flex'} text-[20px] md:text-[24px] sm:text-[20px] font-light leading-relaxed`}>
               Through this ongoing journey, I’ve gained a new sense of purpose. Gradually, I became drawn to ideas that aligned with my evolving values. Now, I’m building Sustenance—a place for women to come together, find support, and share meaningful conversations rooted in faith and growth. It’s the kind of community I wish I had when navigating my own path.
             </h3>
             <Link to={'/about'}>
-            <h3 className={` ${isReverseLayout ? 'hidden' : 'flex'} text-[#A4A4A4] text-[20px] md:text-[22px] hover:text-[#2226bc]`}>
+            <h3 className={` ${isReverseLayout ? 'hidden' : 'flex'} text-[#A4A4A4] text-[20px] md:text-[22px] font-light hover:text-[#2226bc]`}>
               Read more...
             </h3>
           </Link>
@@ -47,10 +48,10 @@ export const HomeContent = () => {
             />
             ) : ( <img
               className="pic-img rounded-full object-cover lg:w-[559px] lg:h-[559px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[70vw] h-auto"
-              src={photo_black} 
+              src={isHovered ? ZW_Yellow : photo_black} 
               alt="About me"
-              // onMouseEnter={() => setIsHovered(true)} 
-              // onMouseLeave={() => setIsHovered(false)} 
+              onMouseEnter={() => setIsHovered(true)} 
+              onMouseLeave={() => setIsHovered(false)} 
             />)}          
           </div>
         </div>
