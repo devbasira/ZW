11876c8d1b4195e868a92fc99d732ceeeebd7fde
Ghazali <shazebansari2536@gmail.com>
2024-11-27 01:02:56 +0530

    Added responsiveness

--- src/components/AnimatedComponent.jsx
+++ src/components/AnimatedComponent.jsx
@@ -11,10 +11,10 @@ const AnimatedSection = () => {
 
   return (
     <div className="bg-[#FBB00A] -z-20 relative grid grid-rows-1 place-content-center">
-      <motion.div className="relative row-span-1 w-[1200px]  h-[1749px] flex justify-center">
-        <div className="absolute top-[15%] -z-30 left-[1%]">
+      <motion.div className="relative row-span-1 w-full lg:w-[1200px] h-screen md:h-[1749px] lg:h-[1749px] flex items-center justify-center">
+        <div className="absolute top-[25%] lg:top-[13%] -z-30 right-48  lg:left-[1%]">
         <motion.h1
-          className="fixed   text-[275px] text-white font-[900] z-10"
+          className="fixed  text-[100px] lg:text-[275px] md:text-[275px] text-white font-[900] z-10"
           style={{
             x: upperTextX,
             opacity: textOpacity,
@@ -27,11 +27,11 @@ const AnimatedSection = () => {
         <motion.img
           src={photo}
           alt="Background"
-          className="fixed -z-25 bottom-0 top-[10%] w-[890px] h-[1200px] object-cover z-0"
+          className="fixed -z-25 bottom-0 top-[13%] w-[450px] h-[600px] md:w-[600px] md:h-[750px] lg:w-[750px] lg:h-[1000px] object-cover z-0"
         />
-        <div className="absolute top-[25%] left-[35%]">
+        <div className="absolute top-[35%] lg:top-[22%] right-24 lg:left-[35%]">
         <motion.h1
-          className="fixed opacity-2  text-[275px] text-white/[.35] font-[900] z-10"
+          className="fixed opacity-2 text-[100px] lg:text-[275px] md:text-[275px]  text-white/[.35] font-[900] z-10"
           style={{
             x: lowerTextX,
             opacity: textOpacity,
