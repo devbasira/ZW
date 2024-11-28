6fe4409a5910b0d86fa489dbc8bedd3489cfb28f
Ghazali <shazebansari2536@gmail.com>
2024-11-28 19:21:29 +0530

    Added responsiveness

--- src/components/AnimatedComponent.jsx
+++ src/components/AnimatedComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import photo from '../assets/zw_photo.png'
+import photo from '../assets/ZW_Yellow.png'
 
 const AnimatedSection = () => {
   const { scrollYProgress } = useScroll();
@@ -12,9 +12,9 @@ const AnimatedSection = () => {
   return (
     <div className="bg-[#FBB00A] -z-20 relative grid grid-rows-1 place-content-center">
       <motion.div className="relative row-span-1 w-full lg:w-[1200px] h-screen md:h-[1749px] lg:h-[1749px] flex items-center justify-center">
-        <div className="absolute top-[25%] lg:top-[13%] -z-30 right-48  lg:left-[1%]">
+        <div className="absolute top-[45%] lg:top-[15%] -z-30 right-48  lg:left-[1%]">
         <motion.h1
-          className="fixed  text-[100px] lg:text-[275px] md:text-[275px] text-white font-[900] z-10"
+          className="fixed text-[100px] lg:text-[275px] md:text-[275px] text-white font-[900] z-10"
           style={{
             x: upperTextX,
             opacity: textOpacity,
@@ -27,9 +27,9 @@ const AnimatedSection = () => {
         <motion.img
           src={photo}
           alt="Background"
-          className="fixed -z-25  bottom-0 -right-7 lg:right-1/4 lg:top-[-5.0%] w-[600px] h-[750px] md:w-[600px] md:h-[750px] lg:w-[850px] lg:h-[1100px] object-cover   z-0"
+          className="fixed -z-25 bottom-0 lg:top-[15%]  w-auto h-[75vh] md:w-[600px] md:h-[750px] lg:w-[570px] lg:h-[850px] object-cover z-0"
         />
-        <div className="absolute top-[35%] lg:top-[22%] right-24 lg:left-[35%]">
+        <div className="absolute top-[52%] lg:top-[25%] right-24 lg:left-[35%]">
         <motion.h1
           className="fixed opacity-2 text-[100px] lg:text-[275px] md:text-[275px]  text-white/[.35] font-[900] z-10"
           style={{
